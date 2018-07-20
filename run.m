c048218df8aacbd16bed3bad28a26348d4c719a5
khoa-Vignesh <38530047+khoa-Vignesh@users.noreply.github.com>
2018-07-20 16:13:19 +0200

    Update run.m

--- run.m
+++ run.m
@@ -25,3 +25,30 @@ subplot(1,2,1)
 imshow(vy)
 subplot(1,2,2)
 imshow(ux)
+
+rows = size(h_down,1);
+columns = size(h_down,2);
+
+x = [];
+y = [];
+for i = 1: rows
+  for j = 1: columns
+      if h_down(i, j) ~= 0
+          x = [x; i];
+          y = [y; j];
+      end
+  end
+end
+
+% [rows, columns] = size(h_down)
+% [x, y] = meshgrid(1:columns, 1:rows)
+% mask = h_down~=0
+% x = x(mask)
+% y = y(mask)
+
+data =[x';y'];
+ figure;plot(data(1,:),data(2,:),'o');
+
+% ransac = ransac(data,num,iter,threshDist,inlierRatio)
+%[bestParameter1,bestParameter2] = ransac(data,3,100,10,0.1);
+
